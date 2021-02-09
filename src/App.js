6c1b25cadb1d7ef8b43c532e09a1d355ca509c01
PatrickBoynton <jpboynton3@gmail.com>
2021-02-09 14:15:03 -0500

    Added a list and Form component.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import './App.css';
 import React, { Component } from 'react';
 
+class ImageForm extends Component {
+  render() {
+    return (
+      <form>
+        <input placeholder="Image Url"/>
+        <input placeholder="Image Caption"/>
+        <button type="submit" className="btn">Add Image</button>
+      </form>
+    )
+  }
+}
+
+class ImageList extends Component {
+  render() {
+    return (
+      <ul>
+        <li><h2>Image Title</h2></li>
+        <li>Image</li>
+      </ul>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <form>
-          <input placeholder="Image Url"/>
-          <input placeholder="Image Caption"/>
-          <button type="submit" className="btn">Add Image</button>
-        </form>
+        <ImageForm/>
+        <ImageList/>
       </React.Fragment>
     )
   }
