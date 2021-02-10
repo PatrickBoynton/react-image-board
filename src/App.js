ef0c721d1d20cdc247b61b0bd028ba3fc50ab085
PatrickBoynton <jpboynton3@gmail.com>
2021-02-10 09:15:35 -0500

    Ran another build.

--- src/App.js
+++ src/App.js
@@ -60,7 +60,11 @@ class App extends Component {
     const images = [
       {
         imgUrl: "https://images.unsplash.com/photo-1493612276216-ee3925520721?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-        imgCaption: "This is a caption!",
+        imgCaption: "A lightbulb against a sunset.",
+      },
+      {
+        imgUrl: "https://images.unsplash.com/photo-1429087969512-1e85aab2683d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
+        imgCaption: "A chair with a baloon"
       }
     ]
     this.setState({images});
